7087bd6604611cb8f479e569cc327e071b8e15a7
Georg Brandl <georg@python.org>
2010-02-14 23:07:56 +0100

    Better imenu tag formatting for python.

--- distext/semantic.el
+++ distext/semantic.el
@@ -11,6 +11,11 @@
 (global-set-key (kbd "C-c <left>") 'semantic-tag-folding-fold-block)
 (global-set-key (kbd "C-c <right>") 'semantic-tag-folding-show-block)
 
+(defun semantic-format-tag-name-short (tag &optional parent color)
+  (let ((name (semantic-format-tag-name tag parent color)))
+    (if (string-prefix-p "\"\"\"" name) "<doc>" name)))
+(setq semantic-imenu-summary-function 'semantic-format-tag-name-short)
+
 ;; enable semantic features for Python mode
 (add-hook 'python-mode (lambda ()
   (add-hook 'after-save-hook 'semantic-fetch-tags nil t)
@@ -18,5 +23,6 @@
 
 (add-hook 'semantic-init-hooks (lambda ()
   (when (eq major-mode 'python-mode)
+    (semantic-fetch-tags)
     (semantic-stickyfunc-mode 1)
     (semantic-highlight-func-mode 1))) t)
