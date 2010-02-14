07e021acd2d1a6d05699232b704a8e319b807156
Georg Brandl <georg@python.org>
2010-02-14 22:38:44 +0100

    Make nxhtml less verbose.

--- nxhtml/autostart.el
+++ nxhtml/autostart.el
@@ -39,7 +39,7 @@
 ;;
 ;;; Code:
 
-(message "Nxml/Nxhtml Autostart.el loading ...")
+;(message "Nxml/Nxhtml Autostart.el loading ...")
 
 (defconst nxhtml-menu:version "2.05")
 ;;(setq message-log-max t)
@@ -166,7 +166,7 @@ them."
     (add-to-list 'load-path company-dir)
     (add-to-list 'load-path tests-dir)
 
-    (message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
+    ;(message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
 
     ;; Autoloading etc
 
@@ -174,22 +174,22 @@ them."
     ;;(unless noninteractive (require 'as-external))
 
     (load (expand-file-name "nxhtml-loaddefs" nxhtml-install-dir))
-    (message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
+    ;(message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
 
     ;; Turn on `nxhtml-global-minor-mode' unconditionally
     (nxhtml-global-minor-mode 1)
-    (message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
+    ;(message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
 
     ;; Patch the rnc include paths
     (when (fboundp 'nxml-mode)
       (load (expand-file-name "etc/schema/schema-path-patch"
                               nxhtml-install-dir))
       (rncpp-patch-xhtml-loader))
-    (message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
+    ;(message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
 
     ;; Load nXhtml
     (load (expand-file-name "nxhtml/nxhtml-autoload" nxhtml-install-dir)))
-  (message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
+  ;(message "... nXhtml loading %.1f seconds elapsed ..." (- (float-time) nxhtml-load-time-start))
 
   ;; Flymake, this may break some users setup initially, but I see no better way...
   (when nxhtml-flymake-setup
@@ -198,10 +198,10 @@ them."
     (add-hook 'flymake-mode-hook 'flymake-init-load-flymakemsg))
 
   ;; Tell what have been loaded of nXhtml:
-  (nxhtml-list-loaded-features nil)
+  ;(nxhtml-list-loaded-features nil)
 
   ;; How long time did it all take?
-  (message "Nxml/Nxhtml Autostart.el loaded in %.1f seconds" (- (float-time) nxhtml-load-time-start))
+  ;(message "Nxml/Nxhtml Autostart.el loaded in %.1f seconds" (- (float-time) nxhtml-load-time-start))
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
