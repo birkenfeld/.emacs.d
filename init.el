ddd9e2f22c79f26bef871cd8c723a47f904a7bc5
Georg Brandl <georg@python.org>
2010-05-18 16:24:30 +0000

    Add hook to kill minibuffer on mouseclicking another buffer.

--- init.el
+++ init.el
@@ -312,6 +312,13 @@
           '(lambda ()
              (define-key minibuffer-local-map "\t" 'comint-dynamic-complete)))
 
+;; kill minibuffer when changing buffer by mouseclick
+(defun stop-using-minibuffer ()
+  (when (and (>= (recursion-depth) 1)
+             (active-minibuffer-window))
+    (abort-recursive-edit)))
+(add-hook 'mouse-leave-buffer-hook 'stop-using-minibuffer)
+
 
 ;; ---------- Elisp mode specifics ---------------------------------------------
 
