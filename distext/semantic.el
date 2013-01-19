510c0c54279dbe17e50b8ce47b1e34e13b67067c
Georg Brandl <georg@python.org>
2013-01-19 08:16:32 +0100

    Disable semantic and nxhtml, add web-mode

--- distext/semantic.el
+++ distext/semantic.el
@@ -1,31 +1,31 @@
 ;; ---------- semantic.el init file: load semantic if present ------------------
 
-;; semantic
-(setq semantic-load-turn-useful-things-on t)
-(require 'semantic)
-;(require 'semanticdb)
-(require 'semantic/db)
-;(semantic-load-enable-code-helpers)
+;; ;; semantic
+;; (setq semantic-load-turn-useful-things-on t)
+;; (require 'semantic)
+;; ;(require 'semanticdb)
+;; (require 'semantic/db)
+;; ;(semantic-load-enable-code-helpers)
 
-;(require 'semantic-tag-folding)
-;(global-semantic-tag-folding-mode 1)
-;(global-set-key (kbd "C-c <left>") 'semantic-tag-folding-fold-block)
-;(global-set-key (kbd "C-c <right>") 'semantic-tag-folding-show-block)
+;; ;(require 'semantic-tag-folding)
+;; ;(global-semantic-tag-folding-mode 1)
+;; ;(global-set-key (kbd "C-c <left>") 'semantic-tag-folding-fold-block)
+;; ;(global-set-key (kbd "C-c <right>") 'semantic-tag-folding-show-block)
 
-(defun semantic-format-tag-name-short (tag &optional parent color)
-  (let ((name (semantic-format-tag-name tag parent color)))
-    (if (string-prefix-p "\"\"\"" name) "<doc>" name)))
-(setq semantic-imenu-summary-function 'semantic-format-tag-name-short)
+;; (defun semantic-format-tag-name-short (tag &optional parent color)
+;;   (let ((name (semantic-format-tag-name tag parent color)))
+;;     (if (string-prefix-p "\"\"\"" name) "<doc>" name)))
+;; (setq semantic-imenu-summary-function 'semantic-format-tag-name-short)
 
-;; enable semantic features for Python mode
-;(add-hook 'python-mode (lambda ()
-;  (add-hook 'after-save-hook 'semantic-fetch-tags nil t)
-;))
+;; ;; enable semantic features for Python mode
+;; ;(add-hook 'python-mode (lambda ()
+;; ;  (add-hook 'after-save-hook 'semantic-fetch-tags nil t)
+;; ;))
 
-(add-hook 'python-mode-hook #'semantic-mode)
+;; (add-hook 'python-mode-hook #'semantic-mode)
 
-(add-hook 'semantic-init-hooks (lambda ()
-  (when (eq major-mode 'python-mode)
-    (semantic-fetch-tags)
-    (semantic-stickyfunc-mode 1)
-    (semantic-highlight-func-mode 1))) t)
+;; (add-hook 'semantic-init-hooks (lambda ()
+;;   (when (eq major-mode 'python-mode)
+;;     (semantic-fetch-tags)
+;;     (semantic-stickyfunc-mode 1)
+;;     (semantic-highlight-func-mode 1))) t)
--- extensions.el
+++ extensions.el
@@ -226,8 +226,12 @@
 (global-set-key (kbd "C-c b n") 'bm-next)
 (global-set-key (kbd "C-c b p") 'bm-previous)
 
-;; nxhtml
-(load "~/.emacs.d/nxhtml/autostart.el")
+;; web-mode for HTML/Jinja/CSS/JS
+(autoload 'web-mode "web-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ;; talcum-mode: replaces LaTeX commands by Unicode symbols
 (autoload 'talcum-mode "talcum" nil t)
