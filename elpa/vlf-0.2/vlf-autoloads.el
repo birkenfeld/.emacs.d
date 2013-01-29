1e91ca1ae383601fd644256623d80421c67c3d92
Georg Brandl <georg@python.org>
2013-01-29 08:19:17 +0100

    elpa update, add vlf

---
+++ elpa/vlf-0.2/vlf-autoloads.el
@@ -0,0 +1,18 @@
+;;; vlf-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("vlf-pkg.el" "vlf.el") (20732 59857 539811
+;;;;;;  669000))
+
+;;;***
+
+(provide 'vlf-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; vlf-autoloads.el ends here
