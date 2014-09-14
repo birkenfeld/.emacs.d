35b2bb61e5375fb1af340d40af855fc82030ecd6
Georg Brandl <georg@python.org>
2014-09-15 00:05:23 +0200

    finish emacs-goodies-el weedout, move remaining libs to lisp/unused

---
+++ elpa/keywiz-1.4/keywiz-autoloads.el
@@ -0,0 +1,15 @@
+;;; keywiz-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("keywiz.el") (21526 4074 117813 92000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; keywiz-autoloads.el ends here
