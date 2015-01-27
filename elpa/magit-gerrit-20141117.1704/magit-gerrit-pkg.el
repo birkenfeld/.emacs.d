e401494faa81a8d741469fcbac2ef4077aa9141c
Georg Brandl <georg@python.org>
2015-01-27 22:02:10 +0100

    add magit-gerrit

---
+++ elpa/magit-gerrit-20141117.1704/magit-gerrit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "magit-gerrit" "20141117.1704" "Magit plugin for Gerrit Code Review" '((magit "1.2.0")) :url "https://github.com/terranpro/magit-gerrit")
