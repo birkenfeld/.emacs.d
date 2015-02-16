a85c0c8fd7bb6ad93adaa796fd9c5a62e2301688
Georg Brandl <georg@python.org>
2015-02-16 07:25:50 +0100

    add realgud from melpa

---
+++ elpa/loc-changes-20141216.1346/loc-changes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "loc-changes" "20141216.1346" "Helps users and programs keep track of positions even after buffer changes." 'nil :url "http://github.com/rocky/emacs-loc-changes")
