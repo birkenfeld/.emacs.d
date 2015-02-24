124f3a1bed4f336ed905bfe988503fe9ee290144
Georg Brandl <georg@python.org>
2015-02-24 07:25:22 +0100

    elpa update

---
+++ elpa/load-relative-20150221.1345/load-relative-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "load-relative" "20150221.1345" "relative file load (within a multi-file Emacs package)" 'nil :url "http://github.com/rocky/emacs-load-relative" :keywords '("internal"))
