10a0353eabfb08bdd0153731be0e0fb54e666641
Georg Brandl <g.brandl@fz-juelich.de>
2015-05-13 10:29:42 +0200

    elpa update

---
+++ elpa/magit-gerrit-20150511.1820/magit-gerrit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "magit-gerrit" "20150511.1820" "Magit plugin for Gerrit Code Review" '((magit "1.2.0")) :url "https://github.com/terranpro/magit-gerrit")
