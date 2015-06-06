cf1aedd4d083d4cfbbdf2c67e78be91e519c5f09
Georg Brandl <georg@python.org>
2015-06-06 14:15:43 +0200

    more rust

--- setup/rust.el
+++ setup/rust.el
@@ -1,5 +1,10 @@
 ;; Settings and setup for Rust mode
 
+(autoload 'rust-mode "rust-mode")
+(setq auto-mode-alist
+      (append '(("\\.rs$" . rust-mode))
+              auto-mode-alist))
+
 (add-hook 'rust-mode-hook #'flycheck-rust-setup)
 
 ; this does add-hook already
