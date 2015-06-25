62a206b9c26127a19b181c35d5557af2306fbcef
Georg Brandl <georg@python.org>
2015-06-25 20:54:30 +0200

    elpa update

--- elpa/try-20141116.1631/try-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "try" "20141116.1631" "Try out Emacs packages." '((emacs "24")) :url "http://github.com/larstvei/try" :keywords '("packages"))
---
+++ elpa/try-20150608.211/try-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "try" "20150608.211" "Try out Emacs packages." '((emacs "24")) :url "http://github.com/larstvei/try" :keywords '("packages"))
