ce68944d5468676f67a849affbc51cb347cd73af
Georg Brandl <georg@python.org>
2015-07-05 15:36:47 +0200

    rust: prettify symbols :)

--- setup/rust.el
+++ setup/rust.el
@@ -11,12 +11,22 @@
   (interactive)
   (compile compile-command))
 
+(defun my-rust-mode-hook ()
+  (push '("::" . ?∷) prettify-symbols-alist)
+  (push '("!=" . ?≠) prettify-symbols-alist)
+  (push '("<=" . ?≤) prettify-symbols-alist)
+  (push '(">=" . ?≥) prettify-symbols-alist)
+  (push '("&&" . ?∧) prettify-symbols-alist)
+  (push '("||" . ?∨) prettify-symbols-alist)
+  (prettify-symbols-mode 1))
+
 (eval-after-load "rust-mode"
   '(progn
 
      ;; Racer setup: this does add-hook already
      (require 'racer)
-     (add-hook 'rust-mode-hook 'racer-activate)
+     (add-hook 'rust-mode-hook #'racer-activate)
+     (add-hook 'rust-mode-hook #'my-rust-mode-hook)
 
      (define-key rust-mode-map (kbd "TAB") 'racer-complete-or-indent)
      (define-key rust-mode-map (kbd "M-.") 'racer-find-definition)
