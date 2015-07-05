db800cfe3505c3caf9fd3c0120ddbba842246719
Georg Brandl <georg@python.org>
2015-07-05 15:39:34 +0200

    elpa update

--- elpa/test-case-mode-20140123.1115/test-case-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "test-case-mode" "20140123.1115" "unit test front-end" 'nil :url "http://nschum.de/src/emacs/test-case-mode/" :keywords '("tools"))
---
+++ elpa/toml-mode-20150702.2125/toml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "toml-mode" "20150702.2125" "Mojor mode for editing TOML files" 'nil :url "https://github.com/dryman/toml-mode.el" :keywords '("data" "toml"))
