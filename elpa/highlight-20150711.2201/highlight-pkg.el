025618959a772506b7662af910ec801d934b7a89
Georg Brandl <georg@python.org>
2015-08-10 20:25:28 +0200

    elpa update

--- elpa/highlight-20150711.2201/highlight-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "highlight" "20150711.2201" "Highlighting commands." 'nil :url "http://www.emacswiki.org/highlight.el" :keywords '("faces" "help" "local"))
