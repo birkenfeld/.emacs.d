581eeb2d23aa532f029952d2c31151e1300cc3d3
Georg Brandl <georg@python.org>
2015-09-07 21:02:38 +0200

    elpa update

--- elpa/web-mode-20150830.943/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20150830.943" "major mode for editing web templates" 'nil :stars '(#("783" 0 3 (font-lock-face paradox-star-face))) :url "http://web-mode.org" :keywords '("languages"))
---
+++ elpa/web-mode-20150831.1145/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web-mode" "20150831.1145" "major mode for editing web templates" 'nil :stars '(#("786" 0 3 (font-lock-face paradox-star-face))) :url "http://web-mode.org" :keywords '("languages"))
