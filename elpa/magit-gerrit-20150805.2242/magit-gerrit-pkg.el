26fcf78964fc029f6cda1eca62f7f6575b64924d
Georg Brandl <georg@python.org>
2015-09-12 11:14:59 +0200

    elpa update

--- elpa/magit-gerrit-20150805.2242/magit-gerrit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "magit-gerrit" "20150805.2242" "Magit plugin for Gerrit Code Review" '((magit "2.1.0")) :url "https://github.com/terranpro/magit-gerrit")
