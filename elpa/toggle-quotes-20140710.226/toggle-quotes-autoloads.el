e4de5b4cce8c3ea2d06d39fd77a194f100eb0976
Georg Brandl <georg@python.org>
2015-09-20 10:52:47 +0200

    add toggle-quotes from elpa

---
+++ elpa/toggle-quotes-20140710.226/toggle-quotes-autoloads.el
@@ -0,0 +1,22 @@
+;;; toggle-quotes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "toggle-quotes" "toggle-quotes.el" (22014 29049
+;;;;;;  346597 51000))
+;;; Generated autoloads from toggle-quotes.el
+
+(autoload 'toggle-quotes "toggle-quotes" "\
+Toggle between single quotes and double quotes.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; toggle-quotes-autoloads.el ends here
---
+++ elpa/toggle-quotes-20140710.226/toggle-quotes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "toggle-quotes" "20140710.226" "Toggle between single and double quoted string" 'nil :stars '(#("9" 0 1 (font-lock-face paradox-star-face))) :url "https://github.com/toctan/toggle-quotes.el" :keywords '("convenience" "quotes"))
