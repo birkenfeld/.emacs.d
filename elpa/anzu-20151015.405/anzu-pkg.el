80d65d224d475ca87e63f6c8e9c7124314af8b10
Georg Brandl <georg@python.org>
2016-01-06 11:00:38 +0100

    elpa update

--- elpa/anzu-20151015.405/anzu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "anzu" "20151015.405" "Show number of matches in mode-line while searching" '((cl-lib "0.5") (emacs "24")) :stars '(#("145" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/syohex/emacs-anzu")
---
+++ elpa/git-timemachine-20160105.306/git-timemachine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-timemachine" "20160105.306" "Walk through git revisions of a file" 'nil :stars '(#("282" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/pidu/git-timemachine" :keywords '("git"))
