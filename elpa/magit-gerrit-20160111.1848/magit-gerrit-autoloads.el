49e9bc7140c9d463d7e171f3396cff7fef1c9545
Georg Brandl <georg@python.org>
2016-01-30 17:22:27 +0100

    elpa update

--- elpa/magit-gerrit-20160111.1848/magit-gerrit-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-gerrit-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("magit-gerrit.el") (22171 54697 969348
-;;;;;;  794000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-gerrit-autoloads.el ends here
