e0d5b8541f25b4fac5ba06f2fc827d3cf419b545
Georg Brandl <georg@python.org>
2016-02-10 11:27:42 +0100

    elpa update

---
+++ elpa/which-key-20160209.832/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "which-key" "20160209.832" "Display available keybindings in popup" '((emacs "24.3")) :stars '(#("155" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/justbur/emacs-which-key")
