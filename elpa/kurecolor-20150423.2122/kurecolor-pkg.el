cba2c7225ff7b7bbefe355b6aa97807e00eb9f20
Georg Brandl <georg@python.org>
2016-02-14 23:10:28 +0100

    add kurecolor

---
+++ elpa/kurecolor-20150423.2122/kurecolor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kurecolor" "20150423.2122" "color editing goodies for Emacs" '((emacs "24.1") (s "1.0")) :stars '(#("42" 0 2 (font-lock-face paradox-star-face))))
