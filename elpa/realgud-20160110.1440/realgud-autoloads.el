af00568897da2490be96a1ad521dcd47525d17ff
Georg Brandl <georg@python.org>
2016-02-22 10:51:24 +0100

    elpa update

--- elpa/realgud-20160110.1440/realgud-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; realgud-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("realgud-pkg.el" "realgud.el") (22171
-;;;;;;  54688 879404 972000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; realgud-autoloads.el ends here
