7d419c4df431c6acd220dc588934dca98a59f160
Georg Brandl <georg@python.org>
2016-05-14 08:39:20 +0200

    elpa update

---
+++ elpa/s-20160508.2357/s-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "s" "20160508.2357" "The long lost Emacs string manipulation library." 'nil :stars '(#("338" 0 3 (font-lock-face paradox-star-face))) :keywords '("strings"))
