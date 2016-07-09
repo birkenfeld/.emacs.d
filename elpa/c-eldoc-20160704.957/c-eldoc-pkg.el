c2f4a89f87617c7567f15ae58c39b4adf08cea0b
Georg Brandl <georg@python.org>
2016-07-09 08:05:25 +0200

    elpa update

---
+++ elpa/c-eldoc-20160704.957/c-eldoc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "c-eldoc" "20160704.957" "helpful description of the arguments to C functions" 'nil :stars '(#("11" 0 2 (font-lock-face paradox-star-face))) :url "http://github.com/nflath/c-eldoc")
--- elpa/ido-ubiquitous-20160618.1304/ido-ubiquitous-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-ubiquitous" "20160618.1304" "Use ido (nearly) everywhere." '((emacs "24.1") (ido-completing-read+ "3.14") (cl-lib "0.5")) :stars '(#("118" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("convenience" "completion" "ido"))
