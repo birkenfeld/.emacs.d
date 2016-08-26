3d9c06832385cc871eca00da0c2bdcfe6c5cf558
Georg Brandl <georg@python.org>
2016-08-26 17:51:21 +0200

    elpa update

---
+++ elpa/digit-groups-20160816.1926/digit-groups-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "digit-groups" "20160816.1926" "Highlight place-value positions in numbers" '((dash "2.11.0")) :stars '("") :url "http://bitbucket.com/adamsmd/digit-groups")
