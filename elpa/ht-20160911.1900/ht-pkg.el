9e315c1b5ab1fa7cd1cc088215d0a7e6efa34ab1
Georg Brandl <georg@python.org>
2016-09-18 08:32:30 +0200

    elpa update

---
+++ elpa/ht-20160911.1900/ht-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ht" "20160911.1900" "The missing hash table library for Emacs" '((dash "2.12.0")) :stars '(#("63" 0 2 (font-lock-face paradox-star-face))) :keywords '("hash table" "hash map" "hash"))
