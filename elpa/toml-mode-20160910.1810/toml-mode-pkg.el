d4844eb1199ad97d9c374a8d86616d7974c78859
Georg Brandl <g.brandl@fz-juelich.de>
2016-11-17 15:01:12 +0100

    elpa update

--- elpa/toml-mode-20160910.1810/toml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "toml-mode" "20160910.1810" "Major mode for editing TOML files" 'nil :stars '(#("13" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/dryman/toml-mode.el" :keywords '("data" "toml"))
