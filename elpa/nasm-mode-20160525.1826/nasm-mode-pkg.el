0103d1601513b5e5e72b34983826005f1ed74942
Georg Brandl <georg@python.org>
2017-01-05 20:54:46 +0100

    elpa update

--- elpa/nasm-mode-20160525.1826/nasm-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "nasm-mode" "20160525.1826" "NASM x86 assembly major mode" '((emacs "24.3")) :stars '(#("13" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/skeeto/nasm-mode")
