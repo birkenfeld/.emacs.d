1a8c5b79d56dbbf1cbbbf2a7182375d05b21a0a1
Georg Brandl <g.brandl@fz-juelich.de>
2017-03-14 08:49:17 +0100

    elpa update

---
+++ elpa/try-20170226.805/try-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "try" "20170226.805" "Try out Emacs packages." '((emacs "24")) :stars '(#("18" 0 2 (font-lock-face paradox-star-face))) :url "http://github.com/larstvei/try" :keywords '("packages"))
