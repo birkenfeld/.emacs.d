bf36a72db60d10946839e0e88da13d697a82327d
Georg Brandl <georg@python.org>
2017-04-01 18:26:51 +0200

    elpa update

---
+++ elpa/flycheck-rust-20170331.1210/flycheck-rust-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-rust" "20170331.1210" "Flycheck: Rust additions and Cargo support" '((emacs "24.1") (flycheck "0.20") (dash "2.13.0") (seq "2.15")) :stars '(#("26" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/flycheck/flycheck-rust" :keywords '("tools" "convenience"))
