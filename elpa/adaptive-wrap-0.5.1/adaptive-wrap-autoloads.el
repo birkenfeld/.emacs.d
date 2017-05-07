66c9270df625601f514e348360852d1f52372760
Georg Brandl <georg@python.org>
2017-05-07 11:04:14 +0200

    elpa update

--- elpa/adaptive-wrap-0.5.1/adaptive-wrap-autoloads.el
+++ elpa/adaptive-wrap-0.5.1/adaptive-wrap-autoloads.el
@@ -3,14 +3,15 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "adaptive-wrap" "adaptive-wrap.el" (22208 61716
-;;;;;;  802465 114000))
+;;;### (autoloads nil "adaptive-wrap" "adaptive-wrap.el" (0 0 0 0))
 ;;; Generated autoloads from adaptive-wrap.el
 
 (autoload 'adaptive-wrap-prefix-mode "adaptive-wrap" "\
 Wrap the buffer text with adaptive filling.
 
 \(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "adaptive-wrap" '("lookup-key" "adaptive-wrap-")))
 
 ;;;***
 
