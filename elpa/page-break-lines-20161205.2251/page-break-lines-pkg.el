8862bb68da76cb11b7a369f2f0658c7121406e30
Georg Brandl <g.brandl@fz-juelich.de>
2017-05-19 15:44:40 +0200

    elpa update

--- elpa/page-break-lines-20161205.2251/page-break-lines-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "page-break-lines" "20161205.2251" "Display ugly ^L page breaks as tidy horizontal lines" 'nil :stars '(#("41" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/purcell/page-break-lines" :keywords '("convenience" "faces"))
