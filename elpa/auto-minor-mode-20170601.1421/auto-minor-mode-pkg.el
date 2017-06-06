b348d8dd5027a2368e0c210c1654b51a25ac4311
Georg Brandl <georg@python.org>
2017-06-06 18:29:58 +0200

    elpa update

---
+++ elpa/auto-minor-mode-20170601.1421/auto-minor-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-minor-mode" "20170601.1421" "Enable minor modes by file name and contents" '((emacs "24.4")) :stars '("") :commit "92871449782f8679f15573ad178b9c2247da8bfd" :keywords '("convenience"))
