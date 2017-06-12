e8794a81ce68e28a84240f46b82ed5833992d792
Georg Brandl <georg@python.org>
2017-06-12 20:32:59 +0200

    mc-lists update

--- .mc-lists.el
+++ .mc-lists.el
@@ -6,6 +6,7 @@
         autopair-extra-insert-opening
         browse-kill-ring-insert-and-quit
         c-electric-pound
+        c-electric-slash
         comment-or-uncomment-region-dwim
         cua-cut-region
         cua-paste
