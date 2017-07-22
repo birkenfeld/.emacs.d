9c82f60ac5ebc6f5c30b462814b82fb7a591a5fa
Georg Brandl <g.brandl@fz-juelich.de>
2017-07-22 17:26:16 +0200

    elpa update

---
+++ elpa/auto-minor-mode-20170716.717/auto-minor-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-minor-mode" "20170716.717" "Enable minor modes by file name and contents" '((emacs "25")) :stars '("") :commit "06fa6975a9fb171b91a8b8234d65ef72374d64e2" :keywords '("convenience"))
--- elpa/rust-mode-20170606.457/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20170606.457" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :stars '(#("119" 0 3 (font-lock-face paradox-star-face))) :commit "ac6a45e6091c0d542b696b6e13551be7e333264a" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
