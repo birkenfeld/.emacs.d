a3b23be5e4261cab161c1565b2de7fa2beba7496
Georg Brandl <georg@python.org>
2017-07-28 06:39:16 +0200

    elpa update

--- elpa/ido-completing-read+-20170714.1307/ido-completing-read+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-completing-read+" "20170714.1307" "A completing-read-function using ido" '((emacs "24.4") (cl-lib "0.5")) :stars '(#("118" 0 3 (font-lock-face paradox-star-face))) :commit "ae858c4caca75b78be6623ba88e1109a51e5fa3a" :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("ido" "completion" "convenience"))
