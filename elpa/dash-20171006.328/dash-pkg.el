f670c0001140a2db6656e9c04e9f912cd4087b94
Georg Brandl <georg@python.org>
2017-10-08 17:21:29 +0200

    elpa update

---
+++ elpa/dash-20171006.328/dash-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dash" "20171006.328" "A modern list library for Emacs" 'nil :stars '(#("756" 0 3 (font-lock-face paradox-star-face))) :commit "3493fc97cf885adfd08b6e1db8f9af7c34fe9cb1" :keywords '("lists"))
