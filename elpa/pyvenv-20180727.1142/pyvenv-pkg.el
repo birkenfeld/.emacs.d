f97e65e93d84e3742d38509a2afe9736405997cd
Georg Brandl <g.brandl@fz-juelich.de>
2018-10-01 08:43:28 +0200

    elpa update

--- elpa/pyvenv-20180727.1142/pyvenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pyvenv" "20180727.1142" "Python virtual environment interface" 'nil :stars '(#("133" 0 3 (font-lock-face paradox-star-face))) :commit "0ff4351f5e3a63575c0a88b293df9d3ccbad4ed6" :keywords '("python" "virtualenv" "tools") :authors '(("Jorgen Schaefer" . "contact@jorgenschaefer.de")) :maintainer '("Jorgen Schaefer" . "contact@jorgenschaefer.de") :url "http://github.com/jorgenschaefer/pyvenv")
