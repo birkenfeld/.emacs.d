9c919488d979f8945242d60e2eb4abbbbb96e8a0
Georg Brandl <georg@python.org>
2018-10-22 16:23:02 +0200

    elpa update

--- elpa/ialign-20180705.1153/ialign-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ialign" "20180705.1153" "visual align-regexp" '((emacs "24.4")) :stars '(#("40" 0 2 (font-lock-face paradox-star-face))) :commit "e92664e673647826161a19e9cfc327fa8c69ba6e" :keywords '("tools" "editing" "align" "interactive") :authors '(("Michał Kondraciuk" . "k.michal@zoho.com")) :maintainer '("Michał Kondraciuk" . "k.michal@zoho.com") :url "https://github.com/mkcms/interactive-align")
