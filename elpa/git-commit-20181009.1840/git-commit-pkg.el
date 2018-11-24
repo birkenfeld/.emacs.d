c576dbd49c6a55d2287459f86b619aec09081b9b
Georg Brandl <georg@python.org>
2018-11-24 19:25:05 +0100

    elpa update

--- elpa/git-commit-20181009.1840/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20181009.1840" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :stars '(#("3445" 0 4 (font-lock-face paradox-star-face))) :commit "3d033e6f1847bcd67a5a58fa67f79c07a06ead7a" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
