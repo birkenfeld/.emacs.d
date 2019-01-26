a249136c2658f3c2bbefd8bf30f2794a240071e9
Georg Brandl <georg@python.org>
2019-01-26 14:48:30 +0100

    elpa update

--- elpa/page-break-lines-20171210.831/page-break-lines-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "page-break-lines" "20171210.831" "Display ^L page breaks as tidy horizontal lines" '((emacs "24.4")) :stars '(#("93" 0 2 (font-lock-face paradox-star-face))) :commit "fd3b7e38ad8747cd009ead7ef1bb150849ccc693" :keywords '("convenience" "faces") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/page-break-lines")
