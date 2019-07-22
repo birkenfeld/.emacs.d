4411eeb8cf7e14f881877ceff2fe7d47cebe56b0
Georg Brandl <georg@python.org>
2019-07-22 06:54:32 +0200

    elpa update, add docker/verilog/c# modes

---
+++ elpa/docker-compose-mode-20180324.1752/docker-compose-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/gbr/.emacs.d/elpa/docker-compose-mode-20180324.1752/docker-compose-mode.el  -*- no-byte-compile: t -*-
+(define-package "docker-compose-mode" "20180324.1752" "Major mode for editing docker-compose files" '((emacs "24.3") (dash "2.12.0") (yaml-mode "0.0.12")) :stars '(#("33" 0 2 (font-lock-face paradox-star-face))) :commit "c9f131d2c90d652435d407fd36c40feebfed1dad" :keywords '("convenience") :authors '(("Ricardo Martins")) :maintainer '("Ricardo Martins") :url "https://github.com/meqif/docker-compose-mode")
