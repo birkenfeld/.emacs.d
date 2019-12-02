ec75d7a360ceaf2c0aa81f8907c3d6aa71377901
Georg Brandl <g.brandl@fz-juelich.de>
2019-12-02 10:57:43 +0100

    elpa update

---
+++ elpa/rmsbolt-20191128.1657/rmsbolt-autoloads.el
@@ -0,0 +1,34 @@
+;;; rmsbolt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "rmsbolt" "rmsbolt.el" (24036 57165 910576
+;;;;;;  490000))
+;;; Generated autoloads from rmsbolt.el
+
+(autoload 'rmsbolt-starter "rmsbolt" "\
+Setup new file based on the sample STARTER-FILE-NAME.
+
+\(fn LANG-NAME)" t nil)
+
+(autoload 'rmsbolt-mode "rmsbolt" "\
+Toggle rmsbolt-mode.
+
+This mode is enabled both in modes to be compiled and output buffers.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("rmsbolt-java.el" "rmsbolt-pkg.el" "rmsbolt-split.el")
+;;;;;;  (24036 57166 50577 322000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rmsbolt-autoloads.el ends here
