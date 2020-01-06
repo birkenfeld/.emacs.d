009d9aa4843b5d5a464aac6bea51b4d75264c165
Georg Brandl <georg@python.org>
2020-01-06 10:02:01 +0100

    elpa update

---
+++ elpa/kurecolor-20191218.148/kurecolor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kurecolor" "20191218.148" "color editing goodies for Emacs" '((emacs "24.1") (s "1.0")) :stars '(#("68" 0 2 (font-lock-face paradox-star-face))) :commit "f8a51febc4cd2576887d477c88d881a865a62d07" :authors '(("Jason Milkins" . "jasonm23@gmail.com")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com"))
---
+++ elpa/verilog-mode-2019.12.17.268053413/verilog-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "verilog-mode" "2019.12.17.268053413" "major mode for editing verilog source in Emacs" 'nil :stars '("") :keywords '("languages") :authors '(("Michael McNamara" . "mac@verilog.com") ("Wilson Snyder" . "wsnyder@wsnyder.org")) :maintainer '("Michael McNamara" . "mac@verilog.com") :url "https://www.veripool.org")
