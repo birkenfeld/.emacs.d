0b75ccb0c7d4b125b5f07836644fdbe0894f9336
Georg Brandl <georg@python.org>
2020-02-28 18:15:26 +0100

    elpa update

---
+++ elpa/lv-20200227.1301/lv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lv" "20200227.1301" "Other echo area" 'nil :stars '(#("999" 0 3 (font-lock-face paradox-star-face))) :commit "5e29ad0092bf0029e1c5f9635bcfd2df314c49fd" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
