9d6cd18b996479eac1ff556dff6646ccba63e99e
Georg Brandl <georg@python.org>
2020-12-06 16:37:08 +0100

    elpa update

--- elpa/pyvenv-20201124.853/pyvenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pyvenv" "20201124.853" "Python virtual environment interface" 'nil :stars '(#("170" 0 3 (font-lock-face paradox-star-face))) :commit "37e7cb17071461744b73400360ff3251ebee507d" :keywords '("python" "virtualenv" "tools") :authors '(("Jorgen Schaefer" . "contact@jorgenschaefer.de")) :maintainer '("Jorgen Schaefer" . "contact@jorgenschaefer.de") :url "http://github.com/jorgenschaefer/pyvenv")
