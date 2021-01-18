1aac6eb263507aad1b38c9b2808373bddb6a525e
Georg Brandl <georg@python.org>
2021-01-18 20:19:14 +0100

    elpa update

---
+++ elpa/dash-20210118.912/dash-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dash" "20210118.912" "A modern list library for Emacs"
+  '((emacs "24"))
+  :commit "525ede2d24c8e6a6c45b2c90280998b336ea6ccc" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/magnars/dash.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
