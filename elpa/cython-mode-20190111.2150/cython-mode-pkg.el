8467760e2b0659829525902851d7f5142db3442e
Georg Brandl <georg@python.org>
2021-11-20 14:29:43 +0100

    elpa update

--- elpa/cython-mode-20190111.2150/cython-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cython-mode" "20190111.2150" "Major mode for editing Cython files" 'nil :stars '(#("3344" 0 4 (font-lock-face paradox-star-face))) :commit "90736d260c9f3f8851b7d31df55785c8cb1b4af3")
---
+++ elpa/cython-mode-20211111.1407/cython-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cython-mode.el  -*- no-byte-compile: t -*-
+(define-package "cython-mode" "20211111.1407" "Major mode for editing Cython files" 'nil :stars '(#("4048" 0 4 (font-lock-face paradox-star-face))) :commit "1f0f5f360f8835728c2b24f17e1c3434ca81c352")
