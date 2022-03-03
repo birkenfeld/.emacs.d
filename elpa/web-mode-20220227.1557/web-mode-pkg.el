932518c606ca5f43a6e8b0b715643aa58e79f31c
Georg Brandl <georg@python.org>
2022-03-03 07:20:01 +0100

    elpa update

---
+++ elpa/web-mode-20220227.1557/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
+(define-package "web-mode" "20220227.1557" "major mode for editing web templates" '((emacs "23.1")) :stars '(#("1256" 0 4 (font-lock-face paradox-star-face))) :commit "f70277774a725e177774cc81ecbd228792cd6656" :authors '(("François-Xavier Bois")) :maintainer '("François-Xavier Bois" . "fxbois@gmail.com") :keywords '("languages") :url "https://web-mode.org")
