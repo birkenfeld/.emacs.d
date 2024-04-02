01456936491b415e50792accaf50e561d170d426
Georg Brandl <georg@python.org>
2024-04-02 20:09:16 +0200

    elpa update

---
+++ elpa/ialign-20240326.1233/ialign-pkg.el
@@ -0,0 +1,14 @@
+(define-package "ialign" "20240326.1233" "visual align-regexp"
+  '((emacs "24.4"))
+  :commit "764aea597b8c9292da2d91294bb6a70e7ad7d6d7" :authors
+  '(("Michał Krzywkowski" . "k.michal@zoho.com"))
+  :maintainers
+  '(("Michał Krzywkowski" . "k.michal@zoho.com"))
+  :maintainer
+  '("Michał Krzywkowski" . "k.michal@zoho.com")
+  :keywords
+  '("tools" "editing" "align" "interactive")
+  :url "https://github.com/mkcms/interactive-align")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
