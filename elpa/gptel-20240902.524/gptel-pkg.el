fa459c9e1674639b36fd1eff0eac100277ea14c6
Georg Brandl <g.brandl@fz-juelich.de>
2024-09-03 21:14:23 +0200

    elpa update

---
+++ elpa/gptel-20240902.524/gptel-pkg.el
@@ -0,0 +1,16 @@
+(define-package "gptel" "20240902.524" "Interact with ChatGPT or other LLMs"
+  '((emacs "27.1")
+    (transient "0.4.0")
+    (compat "29.1.4.1"))
+  :commit "87dfb2d1de3909e6471666d7009643d7c309a93b" :authors
+  '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers
+  '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainer
+  '("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/karthink/gptel")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/x86-lookup-20240823.1135/x86-lookup-pkg.el
@@ -0,0 +1,13 @@
+(define-package "x86-lookup" "20240823.1135" "jump to x86 instruction documentation"
+  '((emacs "24.3")
+    (cl-lib "0.3"))
+  :commit "0a6e4faceb3c313c3ee0ac4b086326a7553c1d8b" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainers
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainer
+  '("Christopher Wellons" . "wellons@nullprogram.com")
+  :url "https://github.com/skeeto/x86-lookup")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
