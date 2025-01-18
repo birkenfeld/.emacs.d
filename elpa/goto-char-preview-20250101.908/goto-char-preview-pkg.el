f3ef3c23a1a16e4de150bda6d771d38fee126e44
Georg Brandl <georg@python.org>
2025-01-18 11:00:22 +0100

    elpa update

---
+++ elpa/goto-char-preview-20250101.908/goto-char-preview-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "goto-char-preview" "20250101.908"
+  "Preview character when executing `goto-char` command."
+  '((emacs "24.3"))
+  :url "https://github.com/emacs-vs/goto-char-preview"
+  :commit "806baf183ca6f6c88aea7f79752e48c4f2f86c89"
+  :revdesc "806baf183ca6"
+  :keywords '("convenience" "character" "navigation")
+  :authors '(("Jen-Chieh" . "jcs090218@gmail.com"))
+  :maintainers '(("Jen-Chieh" . "jcs090218@gmail.com")))
---
+++ elpa/isearch-project-20250101.1008/isearch-project-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "isearch-project" "20250101.1008"
+  "Incremental search through the whole project."
+  '((emacs "27.1")
+    (f     "0.20.0"))
+  :url "https://github.com/jcs-elpa/isearch-project"
+  :commit "abd8ee560d1843f9ea01e1a823ddeafe9fbb0b21"
+  :revdesc "abd8ee560d18"
+  :keywords '("convenience" "search")
+  :authors '(("Jen-Chieh" . "jcs090218@gmail.com"))
+  :maintainers '(("Jen-Chieh" . "jcs090218@gmail.com")))
