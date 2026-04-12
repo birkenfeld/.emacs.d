c218ba0ea5c5caf50c728e4aae2f3b0235f2651e
Georg Brandl <georg@python.org>
2026-04-12 17:55:07 +0200

    elpa update

---
+++ elpa/copilot-chat-20260401.836/copilot-chat-pkg.el
@@ -0,0 +1,18 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "copilot-chat" "20260401.836"
+  "Copilot chat interface."
+  '((emacs         "30.1")
+    (aio           "1.0")
+    (request       "0.3.2")
+    (transient     "0.8.3")
+    (polymode      "0.2.2")
+    (org           "9.4.6")
+    (markdown-mode "2.6")
+    (shell-maker   "0.76.2")
+    (mcp           "0.1.0"))
+  :url "https://github.com/chep/copilot-chat.el"
+  :commit "444c7a5114ba483c3c37fb95ea2888c5539fc9c8"
+  :revdesc "444c7a5114ba"
+  :keywords '("convenience" "tools")
+  :authors '(("cedric.chepied" . "cedric.chepied@gmail.com"))
+  :maintainers '(("cedric.chepied" . "cedric.chepied@gmail.com")))
---
+++ elpa/gptel-20260410.711/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20260410.711"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "8d6411b5f89d796c817ff79324973b8910e164fe"
+  :revdesc "8d6411b5f89d"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
